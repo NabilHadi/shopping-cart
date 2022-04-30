67377dbb958eb3e6d1c54c3774206ebcc43f6c04
Nabil Hadi <napilfadl@gmail.com>
2022-05-01 02:03:59 +0300

    add ProductCard Component with tests

---
+++ src/components/ProductCard.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const numRegExp = /^\b[1-9][0-9]*\b$/;
+
+const ProductCard = ({ productName, productPic, productAlt }) => {
+  const [inputValue, setInputValue] = useState(1);
+
+  const handleOnChange = (e) => {
+    if (!numRegExp.test(e.target.value) && e.target.value !== "") return;
+    setInputValue(e.target.value);
+  };
+
+  const handleIncreaseBtnClick = () => {
+    setInputValue(inputValue + 1);
+  };
+
+  const handleDecreaseBtnClick = () => {
+    setInputValue(inputValue - 1);
+  };
+
+  const handleAddToCardBtnClick = () => {
+    setInputValue(1);
+  };
+
+  return (
+    <div>
+      <div>{productName}</div>
+      <div>
+        <img src={productPic} alt={productAlt} />
+      </div>
+      <div>
+        <input
+          type="text"
+          inputMode="numeric"
+          placeholder="1"
+          pattern="[0-9]"
+          value={inputValue}
+          onChange={handleOnChange}
+          aria-label="Product count"
+        />
+        <button
+          type="button"
+          aria-label="increase product count"
+          onClick={handleIncreaseBtnClick}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          aria-label="decrease product count"
+          onClick={handleDecreaseBtnClick}
+        >
+          -
+        </button>
+      </div>
+      <button
+        type="button"
+        aria-label="add to cart"
+        onClick={handleAddToCardBtnClick}
+      >
+        Add to cart
+      </button>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/tests/ProductCard.test.js
@@ -0,0 +1,171 @@
+import { unmountComponentAtNode } from "react-dom";
+import { render } from "@testing-library/react";
+import ProductCard from "../components/ProductCard";
+import userEvent from "@testing-library/user-event";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe("ProductCard", () => {
+  it("renders a card with correct information", () => {
+    const { getByText, getByAltText, getByRole } = render(
+      <ProductCard
+        productName="test1"
+        productPic="imgsrc"
+        productAlt="testAlt"
+      />,
+      { container }
+    );
+
+    expect(getByText("test1")).toBeDefined();
+    expect(getByAltText("testAlt")).toBeDefined();
+    expect(getByRole("img", { name: "testAlt" })).toBeDefined();
+    expect(getByRole("img", { name: "testAlt" }).getAttribute("src")).toBe(
+      "imgsrc"
+    );
+  });
+
+  it("renders input element with value of 1", () => {
+    const { getByRole } = render(
+      <ProductCard
+        productName="test1"
+        productPic="imgsrc"
+        productAlt="testAlt"
+      />,
+      { container }
+    );
+
+    expect(getByRole("textbox", { name: /product count/i }).value).toMatch(/1/);
+  });
+
+  it("input element accepts only positve numbers", () => {
+    const { getByRole } = render(
+      <ProductCard
+        productName="test1"
+        productPic="imgsrc"
+        productAlt="testAlt"
+      />,
+      { container }
+    );
+
+    const input = getByRole("textbox");
+
+    userEvent.type(input, "{selectall}{backspace}hello1");
+    expect(getByRole("textbox", { name: /product count/i }).value).toMatch(
+      /^\b1\b$/
+    );
+
+    userEvent.type(input, "3");
+    expect(getByRole("textbox", { name: /product count/i }).value).toMatch(
+      /^\b13\b$/
+    );
+
+    userEvent.type(input, "faa");
+    expect(getByRole("textbox", { name: /product count/i }).value).toMatch(
+      /^\b13\b$/
+    );
+
+    userEvent.type(input, "{selectall}{backspace}0");
+    expect(getByRole("textbox", { name: /product count/i }).value).toMatch(
+      /^$/
+    );
+  });
+
+  it("renders increase and decrease button", () => {
+    const { getByRole } = render(
+      <ProductCard
+        productName="test1"
+        productPic="imgsrc"
+        productAlt="testAlt"
+      />,
+      { container }
+    );
+
+    const increaseBtn = getByRole("button", {
+      name: /increase product count/i,
+    });
+    const decreaseBtn = getByRole("button", {
+      name: /decrease product count/i,
+    });
+
+    expect(increaseBtn).toBeDefined();
+    expect(decreaseBtn).toBeDefined();
+  });
+
+  it("clicking increase and decrease buttons changes the product count", () => {
+    const { getByRole } = render(
+      <ProductCard
+        productName="test1"
+        productPic="imgsrc"
+        productAlt="testAlt"
+      />,
+      { container }
+    );
+
+    const productCountInput = getByRole("textbox", { name: /product count/i });
+    const increaseBtn = getByRole("button", {
+      name: /increase product count/i,
+    });
+    const decreaseBtn = getByRole("button", {
+      name: /decrease product count/i,
+    });
+
+    let currentValue = Number(productCountInput.value);
+    userEvent.click(increaseBtn);
+    expect(Number(productCountInput.value)).toBe(currentValue + 1);
+
+    currentValue = Number(productCountInput.value);
+    userEvent.dblClick(increaseBtn);
+    expect(Number(productCountInput.value)).toBe(currentValue + 2);
+
+    currentValue = Number(productCountInput.value);
+    userEvent.dblClick(decreaseBtn);
+    expect(Number(productCountInput.value)).toBe(currentValue - 2);
+  });
+
+  it("renders Add to cart button", () => {
+    const { getByRole } = render(
+      <ProductCard
+        productName="test1"
+        productPic="imgsrc"
+        productAlt="testAlt"
+      />,
+      { container }
+    );
+
+    const addToCartBtn = getByRole("button", { name: /add to cart/i });
+
+    expect(addToCartBtn).toBeDefined();
+  });
+
+  it("clicking add to cart button resets product count to 1", () => {
+    const { getByRole } = render(
+      <ProductCard
+        productName="test1"
+        productPic="imgsrc"
+        productAlt="testAlt"
+      />,
+      { container }
+    );
+
+    const addToCartBtn = getByRole("button", { name: /add to cart/i });
+    const productCountInput = getByRole("textbox", { name: /product count/i });
+
+    userEvent.type(productCountInput, "{selectall}{backspace}3");
+    expect(productCountInput.value).toMatch(/^\b3\b$/);
+
+    userEvent.click(addToCartBtn);
+    expect(productCountInput.value).toMatch(/^\b1\b$/);
+  });
+});
