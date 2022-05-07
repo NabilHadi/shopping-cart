ac6fce2d9e48444da063d6857f2391a6c456e073
Nabil Hadi <napilfadl@gmail.com>
2022-05-07 07:14:01 +0300

    move BrowserRouter from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import ShopPage from "./components/ShopPage";
 import getProducts from "./products";
 
 function App() {
   return (
-    <BrowserRouter basename="shopping-cart">
-      <div className="App flex justify-center bg-slate-100 min-h-screen">
-        <div className=" max-w-5xl flex-1">
-          <header className="font-monotes text-8xl text-center p-4 bg-white">
-            ICONS
-          </header>
-          <nav className="flex justify-center h-17 px-3 items-center bg-white border-t border-neutral-300">
-            <ul className="p-4">
-              <Link
-                to="/"
-                className="clickable btn font-semibold select-none mr-4"
-              >
-                Home
-              </Link>
-              <Link
-                to="shop"
-                className="clickable btn font-semibold select-none"
-              >
-                Shop
-              </Link>
-            </ul>
-          </nav>
-          <div className="h-6"></div>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route
-              path="/shop"
-              element={<ShopPage products={getProducts()} />}
-            />
-          </Routes>
-        </div>
+    <div className="App flex justify-center bg-slate-100 min-h-screen">
+      <div className=" max-w-5xl flex-1">
+        <header className="font-monotes text-8xl text-center p-4 bg-white">
+          ICONS
+        </header>
+        <nav className="flex justify-center h-17 px-3 items-center bg-white border-t border-neutral-300">
+          <ul className="p-4">
+            <Link
+              to="/"
+              className="clickable btn font-semibold select-none mr-4"
+            >
+              Home
+            </Link>
+            <Link to="shop" className="clickable btn font-semibold select-none">
+              Shop
+            </Link>
+          </ul>
+        </nav>
+        <div className="h-6"></div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shop" element={<ShopPage products={getProducts()} />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
